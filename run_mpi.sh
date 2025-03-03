2a9109f54330573f00ab3bee7b23f18b1275e093
Laura Thompson <ltt@tiger3.princeton.edu>
2025-03-03 09:44:11 -0500

    Updated graphing of pressure

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=det_out_air   # create a short name for your job
-#SBATCH --nodes=2                # node count
-#SBATCH --ntasks=224             # total number of tasks across all nodes
+#SBATCH --nodes=5                # node count
+#SBATCH --ntasks=560             # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --time=00:06:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=05:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-user=ltt@princeton.edu
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --account=mueller
